c2469d4f5a9e9e8a2d3855c2eef31b5dd4701fcd
N1CK999 <noble.nick05@gmail.com>
2026-04-17 20:25:11 +0530

    init react typescript and changed aysync pool logic in main

--- api/main.py
+++ api/main.py
@@ -2,16 +2,21 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from psycopg_pool import AsyncConnectionPool
+from sqlalchemy import pool
 from config.settings import settings
-from api.routes import health, generate, review , auth
+from api.routes import health, generate, review , auth,chapters,projects
 
 
 @asynccontextmanager
 async def lifespan(app):
-    async with AsyncPostgresSaver.from_conn_string(settings.postgres_url_sync) as checkpointer:
-        await checkpointer.setup()
-        app.state.checkpointer = checkpointer
-        yield
+    pool = AsyncConnectionPool(conninfo=settings.postgres_url_sync, kwargs={"autocommit": True}, min_size=1, max_size=4, open=False)
+    await pool.open()
+    checkpointer = AsyncPostgresSaver(pool)
+    await checkpointer.setup()
+    app.state.checkpointer = checkpointer
+    yield
+    await pool.close()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -19,5 +24,7 @@ app.include_router(health.router)
 app.include_router(generate.router)
 app.include_router(review.router)
 app.include_router(auth.router)
+app.include_router(chapters.router)
+app.include_router(projects.router)
 
 
