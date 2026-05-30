18d2755962a97dfc8cf9acf50a74ef2937bcbc4b
N1CK999 <noble.nick05@gmail.com>
2026-05-31 03:14:48 +0530

    These as well

--- src/services/neo4j.py
+++ src/services/neo4j.py
@@ -1,1 +1,18 @@
-# maybe todo will add that you can delete bodes in  between but its hard to do that in vector databases
+import os, asyncio
+from neo4j._async import AsyncGraphDatabase
+
+async def test():
+    uri = os.getenv("NEO4J_URI")
+    user = os.getenv("NEO4J_USERNAME")
+    pwd = os.getenv("NEO4J_PASSWORD")
+    print("Connecting to:", uri)
+    driver = AsyncGraphDatabase.driver(uri, auth=(user, pwd))
+    try:
+        async with driver.session() as session:
+            rec = await session.run("RETURN 1 AS ok")
+            r = await rec.single()
+            print("OK:", r["ok"])
+    finally:
+        await driver.close()
+
+asyncio.run(test())
